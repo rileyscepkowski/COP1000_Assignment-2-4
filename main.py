d66235265a91613bccea4ec78ee7c56a837bf7db
Riley Scepkowski <rscepkowski@hawkmail.hccfl.edu>
2025-04-07 14:15:53 -0400

    git push

--- main.py
+++ main.py
@@ -12,17 +12,17 @@ Dependent_Deduction_Rate = 0.025 #2.5 deduction per dependent
 #Calculate taxes and deductions
 stateTax = salary * State_Tax_Rate
 federalTax = salary * Federal_Tax_Rate
-depdendentDeduction = salary * Dependent_Deduction_Rate * numDependents
+dependentDeduction = salary * Dependent_Deduction_Rate * numDependents
 
 
 # Calculate total withholding and take home pay
-totalWithholding = stateTax + federalTax - depdendentDeduction # Subtract deductions because of dependents
+totalWithholding = stateTax + federalTax - dependentDeduction # Subtract deductions because of dependents
 takeHomePay = salary - totalWithholding
 
 
 # Output statements
 print("State Tax: $" + str(stateTax))
 print("Federal Tax: $" + str(federalTax))
-print("Dependents: $" + str(depdendentDeduction))
+print("Dependents: $" + str(dependentDeduction))
 print("Salary: $" + str(salary))
 print("Take Home Pay: $" + str(takeHomePay))
